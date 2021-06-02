29db0ea76f249d82ecfe2fe29f88b0d89fec0f6e
Rick Kauffman <rick@rickkauffman.com>
2021-06-02 08:44:34 -0700

    new front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 
-const  App = () => {
+const App = () => {
   return (
-    <div >
-      <h1>Images Gallery</h1>
+    <div>
+      <Header title="Images Gallery"/>
     </div>
   );
 }
