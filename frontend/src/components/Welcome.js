b16c08714c921c599c41d6af0601480ed7eb2c4a
Rick Kauffman <rick@rickkauffman.com>
2021-06-03 11:43:56 -0700

    welcome

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
-import { ReactComponent as Logo } from '../images/logo.svg';
+import { Jumbotron, Button } from 'react-bootstrap';
 
-const navbarStyle = {
-  backgroundColor: '#fedcba',
-};
-
-const Header = ({ title }) => {
+const Welcome = ({ title }) => {
   return (
-    <Navbar style={navbarStyle} variant="light">
-      <Container>
-        <Logo style={{ maxWidth: '12rem', maxHeight: '2rem' }} />
-      </Container>
-    </Navbar>
+    <Jumbotron>
+      <h1>Images Gallery</h1>
+      <p>
+        This is a simple application that retrieves photos from Unsplash API. In
+        order to start enter any search term in the input field.
+      </p>
+      <p>
+        <Button variant="primary" href="https://unsplash.com" target="_blank">
+          Learn more
+        </Button>
+      </p>
+    </Jumbotron>
   );
 };
 
-export default Header;
+export default Welcome;
