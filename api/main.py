fb58a9641b881eb2e139a97d022abd957585ecc9
Rick Kauffman <rick@rickkauffman.com>
2021-06-03 16:31:29 -0700

    cors

--- api/main.py
+++ api/main.py
@@ -1,17 +1,22 @@
 import os
 import requests 
 from flask import Flask, request
+from flask_cors import CORS
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path="./.env.local")
 
 UNSPLASH_URL="https://api.unsplash.com/photos/random"
 UNSPLASH_KEY=(os.environ.get("UNSPLASH_KEY", ""))
+DEBUG=bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
   raise EnvironmentError("Please create .env.local file and insert UNSPLASH_KEY")
 
 app = Flask(__name__)
+CORS(app)
+
+app.config["DEBUG"] = DEBUG
 
 @app.route("/new-image")
 def new_image():
